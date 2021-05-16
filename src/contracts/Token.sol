24466b86a5edd532047eeff673c13c1bf37ca806
Andrew <amlevy327@gmail.com>
2021-05-16 09:29:51 -0500

    smart contract tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.5.0;
 
 contract Token {
 
-    string public name;
+    string public name = "Andrew Token";
+    string public symbol = "AML";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
 
-    
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
